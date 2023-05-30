a26d19ad4b8d86d547073e1de017fe515db351cb
Max Burrows <maxajburrows@gmail.com>
2023-05-30 18:55:22 +0200

    Optimise solution

--- Pillars.java
+++ Pillars.java
@@ -1,8 +1,5 @@
 public class Pillars {
     public static int pillars(int numPill, int dist, int width){
-        if (numPill == 1) {
-            return 0;
-        }
-        return (numPill-1)*dist*100+(numPill-2)*width;
+        return (numPill > 1) ? (numPill-1)*dist*100+(numPill-2)*width : 0;
     }
 }
